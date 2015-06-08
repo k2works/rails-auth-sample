a35b633745e5cf1ece963bace35cdf8ca4c0ba03
k2works <kakimomokuri@gmail.com>
2015-06-08 14:50:30 +0900

    rails-auth-sample-5 collection_selectメソッドサンプル提供
    selectメソッドサンプル作成
    collection_selectメソッドサンプル作成

--- app/controllers/dash_board_controller.rb
+++ app/controllers/dash_board_controller.rb
@@ -2,5 +2,11 @@ class DashBoardController < ApplicationController
   def index
     @users = User.all
     @belongs = Belong.all
+
+    if @belong.nil?
+      @belong = Belong.new
+    end
+
+    @selected_belong = Belong.where('id =?',1)
   end
 end
